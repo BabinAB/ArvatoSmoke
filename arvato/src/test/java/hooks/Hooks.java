46b534ceffe203afb9ce9a4f6bf112416e91f807
BabinAB <mo3g87@mail.ru>
2019-10-14 23:16:01 +0300

    small improvements

--- arvato/src/test/java/hooks/Hooks.java
+++ arvato/src/test/java/hooks/Hooks.java
@@ -1,25 +1,27 @@
 package hooks;
 
-import com.codeborne.selenide.Configuration;
+//import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import util.PropertyLoader;
 
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Hooks {
     @Before
     public static void setup() {
-        Configuration.timeout = Long.parseLong(System.getProperty("selenide.timeout", "20000"));
-        Configuration.baseUrl = "http://arvato.ee";
+        timeout = 20000;
+        baseUrl = "http://arvato.ee";
         //Configuration.browserSize = PropertyLoader.loadProperty("browser.size");
-        Configuration.fastSetValue = true;
+        fastSetValue = true;
         open("/");
     }
 
     @After
     public static void clearCache() {
-        WebDriverRunner.clearBrowserCache();
+        clearBrowserCookies();
     }
 }
